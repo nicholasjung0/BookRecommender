582c29089fc00a6520cb163dc43338bbb5f72bfe
nicholasjung0 <nicholasjung0@gmail.com>
2025-02-16 19:40:08 -0500

    put file in wrong folder

--- LoadingIndicator.jsx
+++
@@ -1,9 +0,0 @@
-// File: client/src/components/LoadingIndicator.jsx
-
-import React from 'react';
-
-function LoadingIndicator({ isLoading }) {
-    return isLoading ? <p>Loading...</p> : null;
-}
-
-export default LoadingIndicator;
